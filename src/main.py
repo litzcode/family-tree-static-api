4363ec4473cf7d28b089e70b45b2d94baeb40714
Litzy Xu <litzyxuye@gmail.com>
2021-04-19 05:08:55 +0000

    (feat): added '/member/<int:id>' endpoint

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, Member, children
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -33,19 +32,28 @@ def sitemap():
 @app.route('/all', methods=['GET'])
 def get_all_member():
     all_member = Member.query.order_by(Member.age.desc())
-    all_member = list(map(lambda x: x.serialize(), all_member)) 
+    all_member = list(map(lambda x: x.serialize("Member:"), all_member)) 
     print("GET all_member: ", all_member)
     return jsonify(all_member), 200
 
 @app.route('/member/<int:id>', methods=['GET'])
 def get_single_member(id):
-    member = Member.query.get(id)
+    member = Member.query.get(id).serialize("Member")
     print("GET single member: ", member)
 
-    # query_member_relationship = db.session.query(Member.id, children).filter(children.c.child_id == Member.id).filter(children.c.parent_id == Member.id).all()
-    relationships = member.parents.all()
-    print("GET relationships: ", relationships)
-    return jsonify(member.serialize(), relationships), 200
+    query_children = db.session.query(children).filter(children.c.parent_id == id).all()
+    print("GET children [(child_id, member_id)]: ", query_children) # From console, we can see that 'query_children' returns array of tuples
+    # child_01 = Member.query.get(query_child[0][0])
+    child = list( map(lambda x: Member.query.get(x[0]).serialize("Child"), query_children))
+
+    query_parents = db.session.query(children).filter(children.c.child_id == id).all()
+    print("GET parents [(member_id, parent_id)]: ", query_parents) # From console, we can see that 'query_parents' returns array of tuples
+    # parent_01 = Member.query.get(query_parents[0][1])
+    # parent_02 = Member.query.get(query_parents[1][1])    
+    parents = list( map(lambda x: Member.query.get(x[1]).serialize("Parent"), query_parents))
+    
+    return jsonify(member, child, parents), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# association table for parents<->children
+# association table for children<->parents
 children = db.Table('children',
                 db.Column('child_id', db.Integer, db.ForeignKey('member.id')),
                 db.Column('parent_id', db.Integer, db.ForeignKey('member.id'))
@@ -19,19 +19,27 @@ class Member(db.Model):
     age = db.Column(db.Integer, nullable=False)
     parents = db.relationship('Member',
                           secondary=children,
-                          primaryjoin=(children.c.child_id == id),
-                          secondaryjoin=(children.c.parent_id == id),
+                          primaryjoin=(children.c.child_id == id), # primaryjoin means left side of association table is child_id
+                          secondaryjoin=(children.c.parent_id == id), # secondaryjoin means right side of association table is parent_id
                           backref=db.backref('children', lazy='dynamic'),
                           lazy='dynamic')
+
+                          # for testing relationships, for example to search for the childs of an specific member_id, run:
+                          # $ mysql
+                          # USE example;
+                          # SHOW TABLES;  (to verify tables names in current database 'example')
+                          # SELECT * FROM member, children WHERE member.id = children.parent_id;
     
     def __repr__(self):
         return '<Member %r>' % self.name
 
-    def serialize(self):
+    def serialize(self, member_type): # add member_type parameter to serialize member_type
         return {
+            member_type : {
             "id": self.id,
             "name": self.name,
             "lastname": self.lastname,
             "age": self.age
+            }
         }
         
